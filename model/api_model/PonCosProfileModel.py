cb62335211b613164455ce53331d8ad58ae74ab7
Carlo Centofanti <carlocentos@gmail.com>
2023-11-28 12:27:27 +0100

    Fix some model issues

--- model/api_model/PonCosProfileModel.py
+++ model/api_model/PonCosProfileModel.py
@@ -2,8 +2,10 @@ from flask_restx import fields, Model
 
 PonCosProfileModel = Model('PonCosProfileModel', {
     'name': fields.String(required=False, description="The name of the PonCosProfile."),
+    "priority": fields.Integer(required=False, description="The priority to assign."),
+    "bandwidth_type": fields.String(required=False, enum=["explicit"], description="The cos_type to assign."), #TBD enum types
     'maximum_bw': fields.Integer(required=False, description='The maximum bandwidth'),
     "min_bw": fields.Integer(required=False, description="The minimum bandwidth."),
-    "priority": fields.Integer(required=False, description="The priority to assign."),
-    "bandwidth_type": fields.String(required=False, enum=["expedited", "assured"], description="The cos_type to assign.")
+    "cos_type": fields.Integer(required=False, enum=["expedited", "assured"], description="The minimum bandwidth."),
 })
+"PonCosProfile(Name: ont1_assured, Priority: 2, Bandwidth: Type: explicit, Maximum: 1000000, Minimum: 1000000, COS Type: assured)"
